e17cf7c07e9773ffb4de4d510af356568a07350e
zhaoyonghe <909672941@qq.com>
2019-07-01 10:13:55 +0800

    new solutions

---
+++ CoinChangeII/Solution1.java
@@ -0,0 +1,21 @@
+package CoinChangeII;
+/**
+ * Time Complexity: O(coins.length * amount)
+ * Space Complexity: O(amount)
+ * Runtime: 6ms
+ * Rank: 73.87%
+ */
+public class Solution1 {
+	public int change(int amount, int[] coins) {
+		int[] dp = new int[amount + 1];
+		dp[0] = 1;
+		// use different coins in order
+		// no duplication
+		for (int coin : coins) {
+			for (int i = coin; i <= amount; i++) {
+				dp[i] += dp[i - coin];
+			}
+		}
+		return dp[amount];
+	}
+}
